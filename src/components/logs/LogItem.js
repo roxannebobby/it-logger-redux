487b2608cb35047aa10a2ab301fd4cc0c0cd7dfe
Roxanne Weber <marie.sankofa@gmail.com>
2021-12-09 14:05:37 -0600

    model add log form complete and working

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import Moment from 'react';
+// import Moment from 'react';
 import PropTypes from 'prop-types';
 
 const LogItem = ({ log }) => {
-	console.log(log);
 	return (
-		<>
-			<li className='collection-item'>
-				<div>
-					<a
-						href='#edit-log-modal'
-						className={`modal-trigger ${
-							log.attention ? 'red-text' : 'blue-text'
-						}`}
-					>
-						{log.message}
-					</a>
-				</div>
-			</li>
-		</>
+		<li className='collection-item'>
+			<div>
+				<a
+					href='#edit-log-modal'
+					className={`modal-trigger ${
+						log.attention ? 'red-text' : 'blue-text'
+					}`}
+				>
+					{log.message}
+				</a>
+				<br />
+				<span className='grey-text'>
+					<span className='black-text'>ID #{log.id}</span> last updated by:{' '}
+					<span className='black-text'> {log.tech}</span> on {log.date}
+				</span>
+				<a href='#!' className='secondary-content'>
+					<i className='material-icons grey-text'>delete</i>
+				</a>
+			</div>
+		</li>
 	);
 };
 
